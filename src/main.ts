c85d3e43fcb5987cffcbc7da7d9e3f9135016d1e
Nora Gonzalez <leonorgonzalez1007@gmail.com>
2026-07-12 17:07:36 +0200

    terminado

--- src/main.ts
+++ src/main.ts
@@ -1,102 +1,33 @@
 import "./style.css";
 
-interface MusicalInfo {
-  name: string;
-  year: number;
-  active: boolean;
-  genre: string;
+let turnoActual = 0;
+
+function actualizarPantalla() {
+  const textoTurno = turnoActual.toString().padStart(2, "0");
+
+  document.getElementById("numero-turno")!.innerHTML = textoTurno;
 }
 
-const popRock = "🎵 Pop Rock";
-const rock = "🎸 Rock";
-const hardRock = "🤘 Hard Rock";
-const classical = "🎼 Classical";
+function sumar() {
+  turnoActual++;
+  actualizarPantalla();
+}
+function restar() {
+  turnoActual--;
+  actualizarPantalla();
+}
+function reset() {
+  turnoActual = 0;
+  actualizarPantalla();
+}
 
-const beatles: MusicalInfo = {
-  name: "The Beatles",
-  year: 1960,
-  active: true,
-  genre: popRock,
-};
+const botonSumar = document.getElementById("sumar")!;
+const botonRestar = document.getElementById("restar")!;
+const botonReset = document.getElementById("reset")!;
+botonSumar.addEventListener("click", sumar);
 
-const queen: MusicalInfo = {
-  name: "Queen",
-  year: 1970,
-  active: false,
-  genre: rock,
-};
+botonRestar.addEventListener("click", restar);
 
-const acdc: MusicalInfo = {
-  name: "AC/DC",
-  year: 1973,
-  active: true,
-  genre: hardRock,
-};
+botonReset.addEventListener("click", reset);
 
-const ludwig: MusicalInfo = {
-  name: "Ludwig van Beethoven",
-  year: 1770,
-  active: false,
-  genre: classical,
-};
-
-const rolling: MusicalInfo = {
-  name: "The Rolling Stones",
-  year: 1962,
-  active: true,
-  genre: rock,
-};
-
-console.log(
-  "%c" + beatles.name,
-  "font-size:20px; font-weight:bold; background:green;",
-  "Year: ",
-  beatles.year,
-  "Active :",
-  beatles.active,
-  "Genre :",
-  beatles.genre,
-);
-
-console.log(
-  "%c" + queen.name,
-  "font-size:20px; font-weight:bold; background:green;",
-  "Year: ",
-  queen.year,
-  "Active :",
-  queen.active,
-  "Genre :",
-  queen.genre,
-);
-
-console.log(
-  "%c" + acdc.name,
-  "font-size:20px; font-weight:bold; background:green;",
-  "Year: ",
-  acdc.year,
-  "Active :",
-  acdc.active,
-  "Genre :",
-  acdc.genre,
-);
-
-console.log(
-  "%c" + ludwig.name,
-  "font-size:20px; font-weight:bold; background:green;",
-  "Year: ",
-  ludwig.year,
-  "Active :",
-  ludwig.active,
-  "Genre :",
-  ludwig.genre,
-);
-console.log(
-  "%c" + rolling.name,
-  "font-size:20px; font-weight:bold; background:green;",
-  "Year: ",
-  rolling.year,
-  "Active :",
-  rolling.active,
-  "Genre :",
-  rolling.genre,
-);
+actualizarPantalla();
